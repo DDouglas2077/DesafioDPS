5ec04a1f2aff52c4406d428b095cc6c41b5cab18
MigueUrrutia <163359725+MigueUrrutia@users.noreply.github.com>
2025-02-21 00:26:19 -0600

    Creación de ContactList

---
+++ desafio/src/components/ContactList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Contact } from "./Contact";
+
+export const ContactList = ({ contacts, onDelete, onToggleFavorite }) => {
+  return (
+    <div>
+      {contacts.map((contact) => (
+        <Contact
+          key={contact.id}
+          contact={contact}
+          onDelete={onDelete}
+          onToggleFavorite={onToggleFavorite}
+        />
+      ))}
+    </div>
+  );
+};
