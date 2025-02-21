bb7a45119ed84f3110de81d11834c4089ffc3386
MigueUrrutia <163359725+MigueUrrutia@users.noreply.github.com>
2025-02-21 00:43:51 -0600

    Corrigiendo error  app.jsx

--- desafio/src/App.jsx
+++ desafio/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ContactList } from "./components/ContactList";
+import { Contact } from "./components/Contact";
 
 function App() {
   const [contacts, setContacts] = useState([]);
